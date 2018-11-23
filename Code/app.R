7c5f7bd18800c057faf1d9a93402912848699180
shivam-ratnakar <shivam.ratnakar97@gmail.com>
2018-11-23 14:52:26 +0530

    Add files via upload
    User can select the attributes he wishes to perform analysis on.
    
    Acceptance Criteria
    In order to use the analysis functionality the user has to fill the select boxes corresponding to the metrics needed to perform the chosen analysis, using the drop down menu which would contain the names of column names from the data set uploaded. All the required fields must be filled without any duplicate entries for the analysis to begin.

--- Code/app.R
+++ Code/app.R
@@ -1,11 +1,11 @@
-#-----------------------------------------------------------------------------------------------------
-# Title: Data upload Functionality
-# Author: Shivangi Tak
-#-----------------------------------------------------------------------------------------------------
+
 #Import the following libraries.
 library(shinydashboard)
+library(ggplot2)
+# library(s20x)
 library(dplyr)
-
+# library(ggvis)
+# library(reshape2)
 
 
 #Set the header
@@ -14,8 +14,8 @@ header <- dashboardHeader(title = "Sales BI Tool")
 #Sidebar navigator menu items
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Data", icon = icon("bar-chart-o"), tabName = "data")
-    
+    menuItem("Data", icon = icon("bar-chart-o"), tabName = "data"),
+    menuItem("Attribute Selection", icon = icon("bar-chart-o"), tabName = "attribute_s")
   )
 )
 
@@ -29,7 +29,29 @@ body <- dashboardBody(
                         accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))),
             
             fluidPage(fluidRow(column(12,offset=0, style='overflow-x: scroll',tableOutput('table'))))
-            )
+    ),
+    
+    tabItem("attribute_s",
+            fluidPage(
+              titlePanel(fluidRow(column(9, offset=2))),
+              
+              box(title = "Attribute Selection", status = "primary", solidHeader = T, width = 12,
+                  fluidPage(
+                    fluidRow(
+                      
+                      column(3,offset = 0, style='padding:10px;',
+                             selectInput("CP1","Cost Price",choices = NULL)),
+                      column(3,offset = 1,
+                             style='padding:10px;',selectInput("Quantity1","P1_Quantity",choices = NULL)),
+                      column(3, offset = 1,
+                             style='padding:10px;',selectInput("SP1","P1_SP",choices = NULL )),
+                      column(3, offset = 0,
+                             style='padding:10px;',selectInput("Time","Time",choices = NULL )),
+                      column(3, offset = 1,
+                             style='padding:10px;',selectInput("SP2","P2_SP",choices = NULL )),
+                      column(3, offset = 1,
+                             style='padding:10px;',selectInput("Quantity2","P2_Quantity",choices = NULL ))
+                    )))))
   )) 
 
 shinyApp(
@@ -44,11 +66,32 @@ shinyApp(
     })
     
     observe({
-       df<-contentsrea()
+      df<-contentsrea()
       output$table<-renderTable({ head(df,n =5)},bordered = TRUE,
                                 spacing = 's', width = 'auto', align = 'c',
                                 rownames = TRUE, colnames = TRUE,style='overflow-x: scroll;border: 1px solid #000;')
     })
-   
+    
+    observe({     
+      updateSelectInput(session, "Quantity1", choices = names(data.frame(contentsrea())),selected='NULL')
+    })
+    observe({
+      updateSelectInput(session, "SP1", choices = names(data.frame(contentsrea())),selected='NULL')
+    })
+    observe({
+      updateSelectInput(session, "Time", choices = names(data.frame(contentsrea())),selected='NULL')
+    })
+    observe({
+      updateSelectInput(session, "SP2", choices = names(data.frame(contentsrea())),selected='NULL')
+    })
+    observe({
+      updateSelectInput(session, "Quantity2", choices = names(data.frame(contentsrea())),selected='NULL')
+      
+    })
+    observe({
+      updateSelectInput(session, "CP1", choices = names(data.frame(contentsrea())),selected='NULL')
+      
+    })
+    
   })
 
